cbe4565da1b5db4db0706dffea1e709b0389e5a5
TwinkleRooso <twinkleroosso@gmail.com>
2025-05-22 12:00:51 +0530

    Add files via upload

---
+++ viva.sh
@@ -0,0 +1,137 @@
+Networking and system administration lab viva questions and answers
+
+1. Which command lists files and directories in the current folder?
+
+A. read
+B. ls
+C. pwd
+D. grep
+✅ Answer: ls
+
+
+2.Which command is used to display the first 10 lines of a file?
+
+A. head
+B. tail
+C. cut
+D. wc
+✅ Answer: head
+
+
+3. Which command is used to create a new user in the system?
+
+A. passwd
+B. usermod
+C. useradd
+D. userdel
+✅ Answer: useradd
+
+
+4.Which command displays disk space usage?
+
+A. top
+B. df
+C. ps
+D. chmod
+✅ Answer: df
+
+
+5.Which command changes file ownership?
+
+A. chmod
+B. chown
+C. Mv
+D. cp
+✅ Answer: chown
+
+
+6.Which of the following is the correct usage of the grep command to search for the word "error" in a file named logfile.txt?
+
+A. grep "error" logfile.txt
+B. grep -r "error" logfile.txt
+C. grep --search "error" logfile.txt
+D. grep "logfile.txt" "error"
+✅ Answer: grep "error" logfile.txt
+
+
+7. Which option in grep allows you to search for a case-insensitive match?
+
+A. -i
+B. -v
+C. -r
+D. -x
+✅ Answer: -i
+
+
+8. What does the grep -v "error" logfile.txt command do?
+
+A. It searches for lines that contain "error".
+B. It shows only lines that do not contain "error".
+C. It searches for the word "logfile.txt".
+D. It returns lines containing "logfile.txt".
+✅ Answer: It shows only lines that do not contain "error".
+
+
+9. Which grep option shows line numbers along with the matching lines?
+
+A. -l
+B. -n
+C. -c
+D. -r
+✅ Answer: -n
+
+
+10. How do you use grep to search for multiple patterns at once?
+
+A. grep -e "error" -e "warning" logfile.txt
+B. grep "error|warning" logfile.txt
+C. grep "error, warning" logfile.txt
+D. grep "error" "warning" logfile.txt
+✅ Answer: grep -e "error" -e "warning" logfile.txt
+
+
+11. What does the command grep -c "error" logfile.txt do?
+
+A. It counts the number of lines in logfile.txt that contain the word "error".
+B. It shows the contents of logfile.txt.
+C. It counts the total number of lines in logfile.txt.
+D. It displays all occurrences of the word "error" in logfile.txt.
+✅ Answer: It counts the number of lines in logfile.txt that contain the word "error".
+
+
+12. Which grep option is used to display only the names of files that contain a match?
+
+A. -l
+B. -n
+C. -r
+D. -w
+✅ Answer: -l
+
+
+13. Which command creates a new directory?
+
+A. mkdir
+B. cd
+C. find
+D. ls
+✅ Answer: mkdir
+
+
+14. Which command modifies file permissions?
+
+A. chmod
+B. chown
+C. cp
+D. mv
+✅ Answer: chmod
+
+
+15. Which command removes a file named file.txt?
+
+A. rm file.txt
+B. mv file.txt
+C. cp file.txt
+D. chmod file.txt
+✅ Answer: rm file.txt
+
+
